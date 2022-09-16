d3c39c0c95d66eea9c6201fa3c1d061e89e13d3d
Mel <nykee.sr@gmail.com>
2022-09-16 12:23:43 -0700

    commit 1

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-const Arrow = () => {
-    /* const btn = () => {
-      window.scroll({
-        bottom: document.body.scrollHeight, // or document.scrollingElement || document.body
-        left: 0,
-        behavior: 'smooth',
-      });
-    }; */
-  
-    function mylinkfunction(e) {
-      window.location.href = '#bio';
-  
-      e.preventDefault();
-      e.stopPropagation();
-    }
+function myLink(e) {
+  window.location.href = '#bio';
 
-    const Projects = () => {
-        const Btn = (props) => {
-          return (
-            <div className="col-12 col-sm-6 project-btn">
-              <button className="project-div">
-                <a href={props.url}>
-                  <h4>{props.site}</h4>
-                  <p>{props.desc}</p>
-                </a>
-              </button>
-            </div>
-          );
-        };
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+const projects = () => {
+  const Btn = (props) => {
+    return (
+      <div className="col-12 col-sm-6 project-btn">
+        <button className="project-div">
+          <a href={props.url}>
+            <h4>{props.site}</h4>
+            <p>{props.desc}</p>
+          </a>
+        </button>
+      </div>
+    );
+  };
+};
